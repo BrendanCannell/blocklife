15a8d0d9bd45222ee541d14136f9b2721a71cb00
Brendan Cannell <brendan@cannells.com>
2019-07-08 17:23:34 -0400

    + test/block32.js, test/util/life.js

---
+++ test/util/life.js
@@ -0,0 +1,37 @@
+// Naive reference implementation of Life
+
+export let order = ([x0, y0], [x1, y1]) => y0 !== y1 ? y0 - y1 : x0 - x1
+
+export let neighborhood = ([x, y]) => [
+  [x-1, y+1], [x, y+1], [x+1, y+1],
+  [x-1, y  ],           [x+1, y  ],
+  [x-1, y-1], [x, y-1], [x+1, y-1]
+]
+
+export let next = liveCells => {
+  let isAlive = cell => liveCells.find(c => 0 === order(c, cell))
+
+  var neighbors =
+        liveCells
+        .flatMap(neighborhood)
+        .sort(order)
+
+  var next = []
+
+  while (neighbors.length) {
+    let cell = neighbors.shift()
+    var count = 1
+
+    while (neighbors[0] && 0 === order(cell, neighbors[0])) {
+      neighbors.shift()
+      count++
+    }
+
+    if (count === 3 || (count === 2 && isAlive(cell)))
+      next.push(cell)
+  }
+
+  return next.sort(order)
+}
+
+export default next
