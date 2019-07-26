7c232bab6a9fe45c331db117b8127cb8a151f8cb
Brendan Cannell <brendan@cannells.com>
2019-07-26 16:41:19 -0400

    Initial experiments with rho-contracts

--- test/life.js
+++ test/life.js
@@ -1,3 +1,13 @@
 () => {
-  Bar()
+  describe('Life', () => {
+    it('marshals/unmarshalls a glider', () => {
+      assert.isTrue(L.EqualSet(Life(gliderNW).living(), gliderNW))
+    })
+
+    it('moves a glider across the origin', () => {
+      assert.isTrue(L.EqualSet(
+        Life(gliderNW).next({steps: 4}).living(),
+        gliderNW.map(translate([-1, -1]))))
+    })
+  })
 }
