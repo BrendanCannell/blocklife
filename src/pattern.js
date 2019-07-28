432c288deddb9d5645cc34b7715d447268e9330c
Brendan Cannell <brendan@cannells.com>
2019-07-28 19:42:52 -0400

    + tiny-life.js, pattern.js

---
+++ src/pattern.js
@@ -0,0 +1,4 @@
+export let gliderNW = [[0,0],[1,0],[2,0],[0,1],[1,2]]
+export let gliderNE = [[1,0],[2,0],[0,1],[2,1],[2,2]]
+export let gliderSW = [[0,0],[0,1],[2,1],[0,2],[1,2]]
+export let gliderSE = [[1,0],[2,1],[0,2],[1,2],[2,2]]
