abed4a32af63cc10cf2bfabc467c9986bc93d877
Brendan Cannell <brendan@cannells.com>
2019-11-23 13:07:09 -0500

    - unnecessary function wrapper

--- src/memoized-tree32/leaf.js
+++ src/memoized-tree32/leaf.js
@@ -1,7 +1,6 @@
 import Leaf from "../leaf32"
-import ToCanonicalizeConstructor from "../leaf32/canonical-constructor"
+import CanonicalizeLeafConstructor from "../leaf32/canonical-constructor"
 import {Allocate as AllocateCtx} from "../context"
 let AllocateCtxLeaf = AllocateCtx.Leaf
 let AllocateLeaf = (...args) => AllocateCtxLeaf()(...args)
-let CanonicalizeLeafConstructor = ToCanonicalizeConstructor()
 export default {...Leaf, Allocate: AllocateLeaf, CanonicalizeConstructor: CanonicalizeLeafConstructor}
